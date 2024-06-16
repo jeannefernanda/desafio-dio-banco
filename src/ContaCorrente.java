7d0f5e98806b15c17ca3bb0e69be30641300e93d
Jeanne <jeannefernanda.m@gmail.com>
2024-06-16 11:25:49 -0400

    Initial commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,29 @@
+public class ContaCorrente {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,30 @@
+public class ContaPoupanca {
+    private int agencia;
+    private int numero;
+
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+}
